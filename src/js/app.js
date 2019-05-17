497b5a573e5d8e4f67a094a9ad1fc357abafd5e7
Andy Ball <work@andyball.info>
2019-05-17 16:56:07 +1000

    Updates

--- src/js/app.js
+++ src/js/app.js
@@ -1,13 +1,13 @@
 import { Election } from './modules/election'
 
-const url = "https://interactive.guim.co.uk/firehose/australian_election_2019.json"; // 2019
+var url = "https://interactive.guim.co.uk/firehose/australian_election_2019.json"; // 2019
 
 var app = {
 
     loader: function() {
 
         var xhr = new XMLHttpRequest();
-        xhr.open("GET", url, true);
+        xhr.open("GET", `${url}?t=${new Date().getTime()}`, true);
         xhr.setRequestHeader("Content-type", "application/json");
         xhr.onreadystatechange = function () { 
             if (xhr.readyState == 4 && xhr.status == 200) {
